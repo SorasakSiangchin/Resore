8c27d5b7c23b8fe646f13345bfe6e8bcc5207017
SorasakSiangchin <112231280+SorasakSiangchin@users.noreply.github.com>
2022-10-09 16:14:52 +0700

    Inventory complete

--- client/src/app/layout/PrivateRoute.tsx
+++ client/src/app/layout/PrivateRoute.tsx
@@ -34,7 +34,7 @@ export function PrivateLogin({ children }: { children: JSX.Element }) {
         // Redirect them to the /login page, but save the current location they were
         // trying to go to when they were redirected. This allows us to send them
         // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
+        // than dropping them off on the home page.xxxxxxxxxxxxxxxx
         return <Navigate to={`${path}`} state={{ from: location }} replace />;
     }
     return children;
